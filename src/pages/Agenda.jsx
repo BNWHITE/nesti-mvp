0e0feac6c1e6dcad8364322ac01658448bfcdff3
BNWHITE <blacknwhitemanagement@gmail.com>
2025-12-08 20:59:40 +0100

    Create Agenda.jsx

---
+++ src/pages/Agenda.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Agenda() {
+  return (
+    <div className="card">
+      <h2>Agenda</h2>
+      <p>Calendrier familial à venir (prototype).</p>
+    </div>
+  );
+}
